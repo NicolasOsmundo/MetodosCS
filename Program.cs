e3cd700b6f6f830c9623d182790d42a500551d0c
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 15:25:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MetodosCS
+{
+    public class Curso
+    {
+        static void Main(string[] args)
+        {
+            Curso curso = new Curso();
+            curso.CriarCurso("GTI", "Raphael Risos", 1);
+            Console.WriteLine(curso.Codigo);
+            Console.WriteLine(curso.NomeCurso);
+            Console.WriteLine(curso.NomeInstrutor);
+            Console.Read();
+        }
+        
+
+        int Codigo;
+        string NomeCurso;
+        string NomeInstrutor;
+        int cargaHorario;
+        int minimoAlunos;
+        int maximoALunos;
+        public void CriarCurso(string nome, string instrutor, int codigo)
+        {
+            this.NomeCurso = nome;
+            this.NomeInstrutor = instrutor;
+            this.Codigo = codigo;
+        }
+
+    }
+}   
